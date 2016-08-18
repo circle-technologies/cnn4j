2dac512cdfb60aa463e0feaffc595d0f621dc6b1
Sebastian Sellmair <sebastiansellmair@gmail.com>
2016-08-18 18:10:14 +0200

    Removed GPU support. Using native CPU based nd4j instead.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,9 @@
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
 import org.encog.engine.network.activation.ActivationSigmoid;
 import org.encog.neural.networks.BasicNetwork;
 import org.encog.neural.networks.layers.BasicLayer;
@@ -7,15 +13,21 @@ import org.encog.neural.networks.layers.BasicLayer;
  */
 public class Main {
 
-    public static void main(String[] args){
-        BasicNetwork network = new BasicNetwork();
-        network.addLayer(new BasicLayer(null, true, 2));
-        network.addLayer(new BasicLayer(new ActivationSigmoid(), true, 2));
-        network.addLayer(new BasicLayer(new ActivationSigmoid(), false,1));
-        network.getStructure().finalizeStructure();
-        network.reset();
+    public static void main(String[] args) {
+        MultiLayerConfiguration configuration = new NeuralNetConfiguration.Builder().iterations(1)
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .learningRate(0.01)
+                .weightInit(WeightInit.XAVIER)
+                .list()
+                .layer(0, new DenseLayer.Builder().nIn(3).nOut(3).build())
+                .layer(1, new DenseLayer.Builder().nIn(3).nOut(3).build())
+                .layer(2, new DenseLayer.Builder().nIn(3).nOut(1).build())
+                .pretrain(false)
+                .backprop(true)
+                .build();
 
-
+        MultiLayerNetwork network = new MultiLayerNetwork(configuration);
+        network.init();
 
     }
 }
