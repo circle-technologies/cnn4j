c1a816500e47d7532a8df0a14a6e39e2b425674f
Sebastian Sellmair <sebastiansellmair@gmail.com>
2016-08-18 15:57:51 +0200

    Added encog as neural network library

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,21 @@
+import org.encog.engine.network.activation.ActivationSigmoid;
+import org.encog.neural.networks.BasicNetwork;
+import org.encog.neural.networks.layers.BasicLayer;
+
 /**
  * Created by Sellm on 18.08.2016.
  */
 public class Main {
 
     public static void main(String[] args){
+        BasicNetwork network = new BasicNetwork();
+        network.addLayer(new BasicLayer(null, true, 2));
+        network.addLayer(new BasicLayer(new ActivationSigmoid(), true, 2));
+        network.addLayer(new BasicLayer(new ActivationSigmoid(), false,1));
+        network.getStructure().finalizeStructure();
+        network.reset();
+
+
 
     }
 }
