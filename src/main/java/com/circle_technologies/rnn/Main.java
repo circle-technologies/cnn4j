d09d5af11feb2a616f4189442d8a04ff36678668
Sebastian Sellmair <sebastiansellmair@gmail.com>
2016-08-22 11:29:09 +0200

    Implemented naive data reader

--- src/main/java/com/circle_technologies/rnn/Main.java
+++ src/main/java/com/circle_technologies/rnn/Main.java
@@ -1,20 +1,26 @@
 package com.circle_technologies.rnn;
 
 import com.circle_technologies.caf.logging.Log;
+import com.circle_technologies.rnn.naive.INDArrayReader;
 import com.circle_technologies.rnn.naive.Network;
+
+import java.io.IOException;
 
 /**
  * Created by Sellm on 19.08.2016.
  */
 public class Main {
-    public static void main (String[] args){
+    public static void main (String[] args) throws IOException {
         naive();
     }
 
-    public static void naive(){
+    public static void naive() throws IOException {
         Log.debug("RNN","Initializing naive neural network");
         Network network = new Network();
         network.build();
         Log.debug("RNN","Done");
+
+        INDArrayReader reader = new INDArrayReader();
+        reader.readFile("data/training.json");
     }
 }
---
+++ src/main/java/com/circle_technologies/rnn/naive/INDArrayReader.java
@@ -0,0 +1,56 @@
+package com.circle_technologies.rnn.naive;
+
+import com.circle_technologies.caf.io.IOToolKit;
+import com.circle_technologies.caf.logging.Log;
+import org.deeplearning4j.berkeley.Pair;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * Created by Sellm on 22.08.2016.
+ */
+public class INDArrayReader {
+    ArrayList<Pair<INDArray, INDArray>> mPairList;
+
+
+    public INDArrayReader (){
+        mPairList = new ArrayList<Pair<INDArray, INDArray>>();
+    }
+
+    public List<Pair<INDArray,INDArray>> getData(){
+        return mPairList;
+    }
+
+    public ListIterator<Pair<INDArray,INDArray>> getIterator(){
+        return mPairList.listIterator();
+    }
+
+    public void readFile(String filePath) throws IOException {
+
+
+        JSONArray jsonArray = new JSONArray(IOToolKit.readFileToString(filePath));
+        int length = jsonArray.length();
+        for(int i = 0; i < length; i++){
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+            float[] input = new float[3];
+            input[0] = jsonObject.getInt("second_hand_date")-jsonObject.getInt("initial");
+            input[1] = jsonObject.getInt("mileage");
+            input[2] = (float) jsonObject.getDouble("retail_price");
+
+            float[] output = new float[1];
+            output[0] = (float) jsonObject.getDouble("second_hand_price");
+
+            mPairList.add(new Pair<INDArray, INDArray>(Nd4j.create(input),Nd4j.create(output)));
+
+        }
+
+    }
+}
