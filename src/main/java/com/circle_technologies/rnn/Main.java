fb1f78012cc93706efb31470afb2a5c16189e4a9
Sebastian Sellmair <sebastiansellmair@gmail.com>
2016-08-22 11:43:22 +0200

    Suppressed Warnings in Main.java : WeakerAccess

--- src/main/java/com/circle_technologies/rnn/Main.java
+++ src/main/java/com/circle_technologies/rnn/Main.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 /**
  * Created by Sellm on 19.08.2016.
  */
+@SuppressWarnings("WeakerAccess")
 public class Main {
     public static void main (String[] args) throws IOException {
         naive();
