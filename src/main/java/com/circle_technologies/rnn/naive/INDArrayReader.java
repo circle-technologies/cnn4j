0618219b2bdccc43952661d4f6cd8d66059789be
Sebastian Sellmair <sebastiansellmair@gmail.com>
2016-08-22 11:51:50 +0200

    Commented INDArrayReader

--- src/main/java/com/circle_technologies/rnn/naive/INDArrayReader.java
+++ src/main/java/com/circle_technologies/rnn/naive/INDArrayReader.java
@@ -1,7 +1,7 @@
 package com.circle_technologies.rnn.naive;
 
+import com.circle_technologies.caf.annotation.NonNull;
 import com.circle_technologies.caf.io.IOToolKit;
-import com.circle_technologies.caf.logging.Log;
 import org.deeplearning4j.berkeley.Pair;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -12,23 +12,58 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 
 /**
  * Created by Sellm on 22.08.2016.
+ *
+ * Basic JSON reader vor naive neural network. This reader uses
+ * the (second hand date [MINUS] retail date)<br>
+ * the milage <br>
+ * and the retail price<br>
+ *
+ * as input (in the above specified order)
+ *
+ * The retail price is considered as output variable.
+ * This reader can be use for every network with 3 input neurons
+ * ans only 1 output neuron.
+ *
+ * Be sure: This reader confider's the data file to be small enough to read in a whole.
+ *
+ * This object can be used as {@link Iterable} once the data was read via {@link #readFile(String)}.
+ * This class is ment to work with every {@link org.deeplearning4j.datasets.iterator.AbstractDataSetIterator}<br>
+ *
+ * Was explicitly created for {@link DataIterator} implementation.
+ *
  */
 public class INDArrayReader implements Iterable<Pair<INDArray,INDArray>>{
+
+    /**
+     * The data stored in a array list. This is never null.
+     */
+    @NonNull
     ArrayList<Pair<INDArray, INDArray>> mPairList;
+
 
 
     public INDArrayReader (){
         mPairList = new ArrayList<Pair<INDArray, INDArray>>();
     }
 
+    /**
+     *
+     * @return The data yet read. This is never null but might be empty before {@link #readFile(String)} was executed
+     * successfully
+     */
     public List<Pair<INDArray,INDArray>> getData(){
         return mPairList;
     }
 
+
+    /**
+     * Reads a file into the stored data. This can be called accumulative to read in multiple chunks at once.
+     * @param filePath The path to th file.
+     * @throws IOException On reading failure.
+     */
     public void readFile(String filePath) throws IOException {
 
 
