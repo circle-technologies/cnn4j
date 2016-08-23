146f3f8ad0e8f31537c28e96fad037d03a97a868
Sebastian Sellmair <sebastiansellmair@gmail.com>
2016-08-23 13:48:23 +0200

    Implemented CAF 0.10

---
+++ src/main/java/com/circle_technologies/rnn/naive/command/CommandPredict.java
@@ -0,0 +1,14 @@
+package com.circle_technologies.rnn.naive.command;
+
+import com.circle_technologies.rnn.naive.context.NaiveNetworkContext;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+
+/**
+ * Created by Sellm on 23.08.2016.
+ */
+public class CommandPredict  {
+
+
+}
--- src/main/java/com/circle_technologies/rnn/naive/command/CommandTrain.java
+++ src/main/java/com/circle_technologies/rnn/naive/command/CommandTrain.java
@@ -1,8 +1,12 @@
 package com.circle_technologies.rnn.naive.command;
 
+import com.circle_technologies.caf.annotation.Nullable;
 import com.circle_technologies.caf.logging.Log;
 import com.circle_technologies.rnn.naive.context.NaiveNetworkContext;
 import com.circle_technologies.rnn.naive.network.NaiveJSONToINDArray;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
 
 import java.io.IOException;
 
@@ -14,33 +18,41 @@ public class CommandTrain extends AbstractNRNNCommand {
         super(context);
     }
 
+
     @Override
-    public boolean executeCommand(String s) {
-        if (s.toLowerCase().startsWith("train")) {
-            String[] splitted = s.split(" ");
-            if (splitted.length < 4) {
-                Log.info("RNN", "expected more params. eg: train [file] [epochs] [batch size]");
-            } else
+    public String getCommand() {
+        return "train";
+    }
 
-                try {
-                    String file = splitted[1];
-                    int epochs = Integer.parseInt(splitted[2]);
-                    int batch_size = Integer.parseInt(splitted[3]);
+    @Override
+    public Options getOptions() {
+        Options options = new Options();
+        options.addOption(Option.builder().argName("f").longOpt("file").hasArg(true).desc("Path to file").required(true).build());
+        options.addOption(Option.builder().argName("e").longOpt("epochs").hasArg(true).desc("Number of epochs").required(true).type(Integer.class).build());
+        options.addOption(Option.builder().argName("b").longOpt("batches").desc("Batches count").required(true).type(Integer.class).build());
+        return options;
+    }
 
-                    NaiveJSONToINDArray array = new NaiveJSONToINDArray();
-                    array.readFile(file);
-                    getContext().getNetwork().train(array.getListDataSetIterator(batch_size), epochs);
+    @Override
+    public String execute(@Nullable CommandLine commandLine) {
+        String filePath = commandLine.getOptionValue("f");
+        int epochs = Integer.parseInt(commandLine.getOptionValue("e"));
+        int batch = Integer.parseInt(commandLine.getOptionValue("b"));
+
+        try {
+            NaiveJSONToINDArray array = new NaiveJSONToINDArray();
+            array.readFile(filePath);
+            getContext().getNetwork().train(array.getListDataSetIterator(batch), epochs);
 
 
-                } catch (NumberFormatException e) {
-                    Log.info("RNN", "expected epochs param to be an integer");
-                } catch (IOException e) {
-                    Log.info("RNN", "failed reading file");
-                }
-            return true;
+        } catch (NumberFormatException e) {
+            Log.info("RNN", "expected epochs param to be an integer");
+        } catch (IOException e) {
+            Log.info("RNN", "failed reading file");
         }
 
-        return false;
+        return "Training done";
+
     }
 }
 
