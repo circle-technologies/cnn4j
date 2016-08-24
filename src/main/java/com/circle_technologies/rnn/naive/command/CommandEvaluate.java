3e46acbec0b33c0fac0ec76fbcccd7afb5310012
tilligh <gilli.thms@gmail.com>
2016-08-24 15:05:32 +0200

    initial implementation

--- src/main/java/com/circle_technologies/rnn/naive/command/CommandEvaluate.java
+++ src/main/java/com/circle_technologies/rnn/naive/command/CommandEvaluate.java
@@ -2,8 +2,14 @@ package com.circle_technologies.rnn.naive.command;
 
 import com.circle_technologies.caf.annotation.Nullable;
 import com.circle_technologies.rnn.naive.context.NaiveNetworkContext;
+import com.circle_technologies.rnn.naive.network.NaiveNetworkDataAccumulator;
+import com.circle_technologies.rnn.naive.network.norm.NetworkNorm;
 import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
+
+import javax.print.attribute.standard.MediaSize;
+import java.io.IOException;
 
 /**
  * Created by Sellm on 24.08.2016.
@@ -15,16 +21,36 @@ public class CommandEvaluate extends AbstractNRNNCommand {
 
     @Override
     public Options createOptions() {
-        return null;
+        Options options = new Options();
+        options.addOption(Option.builder("f").longOpt("file").desc("File path to the JSON file.").hasArg().required().build());
+        return options;
     }
 
     @Override
     public String getCommand() {
-        return null;
+        return "eval";
     }
 
     @Override
     public String execute(@Nullable CommandLine commandLine) {
-        return null;
+        try {
+            NetworkNorm norm = getContext().getNetworkNorm().get();
+            if (norm == null) {
+                return "Evaluation failed: No network norm found";
+            }
+
+            String filePath = commandLine.getOptionValue("f");
+            NaiveNetworkDataAccumulator accu = new NaiveNetworkDataAccumulator();
+            accu.parseJson(filePath);
+            accu.buildIND(true);
+            accu.normalize(norm);
+            return "Evaluation done";
+        } catch (IOException e) {
+            return "Evaluation failed: IOException";
+        }
+
+
     }
+
+
 }
